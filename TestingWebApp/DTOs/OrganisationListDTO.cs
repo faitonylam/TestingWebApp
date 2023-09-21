a5b2ec55ef272e91af2dab343369adca20144ebd
Tony Lam <tony@msolution.com.hk>
2023-09-21 22:42:29 +0800

    Add xUnit Project, Rearrange Project folder

---
+++ TestingWebApp/DTOs/OrganisationListDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace TestingWebApp.DTOs
+{
+    public class OrganisationListDTO
+    {
+        public int CurrentPage { get; set; } = 1;
+        public int TotalPage { get; set; }
+        public List<Organisation> Data { get; set; } = new List<Organisation>();
+    }
+}
---
+++ TestingWebApp/Models/ViewOrganisation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestingWebApp.Models;
+
+public partial class ViewOrganisation
+{
+    public string OrganisationNumber { get; set; } = null!;
+
+    public string OrganisationName { get; set; } = null!;
+
+    public string AddressLine1 { get; set; } = null!;
+
+    public string? AddressLine2 { get; set; }
+
+    public string? AddressLine3 { get; set; }
+
+    public string? AddressLine4 { get; set; }
+
+    public string? TownName { get; set; }
+
+    public string Postcode { get; set; } = null!;
+
+    public int? EmployeeCount { get; set; }
+}
